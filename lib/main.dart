fe0d49d8b085875997a86c852ba7558d668c3bef
Mariam <you@example.com>
2026-08-06 17:05:23 +0300

    Update README

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,11 @@
 // git commit -m "Update README"
 // git push origin main
 
-import 'package:final_project/sign_in/view.dart';
-import 'package:final_project/widgets/interactive.dart';
-import 'package:final_project/widgets/navigator_bar.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:final_project/profile/view.dart';
-import 'package:final_project/sign_up/view.dart';
 import 'package:flutter/material.dart';
 import 'auth_gate/view.dart';
 import 'firebase_options.dart';
-import 'Home/view.dart';
+
 
 
 
